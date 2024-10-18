7549562a9952cc7d71d24f0bcc68a21f78c7f076
Derek Bolt <derek.bolt@samsara.com>
2024-10-18 17:17:53 +0000

    Updatd neotest config

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,12 +1,26 @@
 return {
   "nvim-neotest/neotest",
-  depenendencies = {
+  dependencies = {
     "fredrikaverpil/neotest-golang",
   },
-  adapters = {
-    ["neotest-golang"] = {
-      go_test_args = { "-v", "-race", "-count=1" },
-      dap_go_enabled = false,
+    -- stylua: ignore
+    keys = {
+      {"<leader>t", "", desc = "+test"},
+      -- cd to the buffers current directory before running the tests, this tis to make it work with large monorepos
+      { "<leader>tt", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+      { "<leader>tT", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
+      { "<leader>tr", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").run.run() end, desc = "Run Nearest" },
+      { "<leader>tl", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").run.run_last() end, desc = "Run Last" },
+      { "<leader>ts", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+      { "<leader>to", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
+      { "<leader>tO", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
+      { "<leader>tS", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").run.stop() end, desc = "Stop" },
+      { "<leader>tw", function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "Toggle Watch" },
+    },
+
+  opts = {
+    adapters = {
+      "neotest-golang",
     },
   },
 }
