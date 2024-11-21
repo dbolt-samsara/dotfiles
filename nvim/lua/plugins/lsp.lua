190f019c332b0e2698e0367fa150d25e3d32b29c
Derek Bolt <derek.bolt@samsara.com>
2024-11-21 10:51:00 -0800

    Updated eslint config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,8 +7,8 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
+    opts = function(_, opts)
+      opts.servers = vim.tbl_extend("force", opts.servers, {
         vtsls = {
           settings = {
             typescript = {
@@ -18,8 +18,28 @@ return {
             },
           },
         },
+      })
+
+      opts.servers = vim.tbl_extend("force", opts.servers, {
         cssls = {},
-      },
-    },
+      })
+
+      -- Eslint
+      -- use .eslintrc-incremental.js if it exists
+      local eslint_config_path = ".eslintrc-incremental.js"
+      local eslint_options = {}
+      if vim.fn.filereadable(eslint_config_path) == 1 then
+        eslint_options = {
+          overrideConfigFile = eslint_config_path,
+        }
+      end
+      opts.servers = vim.tbl_extend("force", opts.servers, {
+        eslint = {
+          settings = {
+            options = eslint_options,
+          },
+        },
+      })
+    end,
   },
 }
