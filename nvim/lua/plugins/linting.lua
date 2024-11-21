958cff0569a1df950ba2d950fa3d0f530126161a
Derek Bolt <derek.bolt@samsara.com>
2024-11-20 16:20:10 -0800

    Updated formatting

--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -3,7 +3,6 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "eslint_d",
         "stylelint",
       },
     },
@@ -12,10 +11,6 @@ return {
     "mfussenegger/nvim-lint",
     linters_by_ft = {
       css = { "stylelint" },
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
     },
   },
 }
