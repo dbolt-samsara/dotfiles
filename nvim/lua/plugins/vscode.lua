13116f51bd73ec4e08da6dea77a0e17aeb45e0c4
Derek Bolt <derek.bolt@samsara.com>
2025-01-17 21:34:12 +0000

    Increased git file change size

--- nvim/lua/plugins/vscode.lua
+++ nvim/lua/plugins/vscode.lua
@@ -41,15 +41,15 @@ return {
           { "n", "<leader>fr", "workbench.action.openRecent" },
           { "n", "<leader>fb", "workbench.action.showAllEditors" },
           { "n", "<leader>,", "workbench.action.showAllEditors" },
-          
+
           -- Search
           { "n", "<leader>sg", "workbench.action.findInFiles" },
           { "n", "<leader>sw", "workbench.action.findInFiles" },
           { "n", "<leader>/", "workbench.action.findInFiles" },
-          
+
           -- File explorer
           { "n", "<leader>e", "workbench.action.toggleSidebarVisibility" },
-          
+
           -- Buffer/Editor/Window management
           { "n", "<leader>w", "workbench.action.files.save" },
           -- Close editor group and window
@@ -59,7 +59,7 @@ return {
           -- Buffer navigation
           { "n", "<S-h>", "workbench.action.previousEditor" },
           { "n", "<S-l>", "workbench.action.nextEditor" },
-          
+
           -- LSP features
           { "n", "gd", "editor.action.revealDefinition" },
           { "n", "gr", "editor.action.goToReferences" },
@@ -67,31 +67,31 @@ return {
           { "n", "K", "editor.action.showHover" },
           { "n", "<leader>ca", "editor.action.quickFix" },
           { "n", "<leader>cr", "editor.action.rename" },
-          
+
           -- Diagnostics
           { "n", "<leader>cd", "editor.action.showHover" },
           { "n", "[d", "editor.action.marker.prev" },
           { "n", "]d", "editor.action.marker.next" },
-           
+
           -- Window splits
           { "n", "<leader>-", "workbench.action.splitEditorDown" },
           { "n", "<leader>|", "workbench.action.splitEditorRight" },
-          
+
           -- Window navigation with conditional behavior
-          { "n", "<C-h>", "workbench.action.navigateLeft",
-          { "n", "<C-j>", "workbench.action.navigateDown",
-          { "n", "<C-k>", "workbench.action.navigateUp",
-          { "n", "<C-l>", "workbench.action.navigateRight",
-          
+          { "n", "<c-h>", "workbench.action.navigateleft" },
+          { "n", "<C-j>", "workbench.action.navigateDown" },
+          { "n", "<C-k>", "workbench.action.navigateUp" },
+          { "n", "<C-l>", "workbench.action.navigateRight" },
+
           -- Panel toggles
-          { "n", "<leader>ue", "workbench.action.toggleSidebarVisibility" },  -- Toggle left sidebar
-          { "n", "<leader>ub", "workbench.action.togglePanel" },              -- Toggle bottom panel
-          { "n", "<leader>ur", "workbench.action.toggleAuxiliaryBar" }        -- Toggle right sidebar
+          { "n", "<leader>ue", "workbench.action.toggleSidebarVisibility" }, -- Toggle left sidebar
+          { "n", "<leader>ub", "workbench.action.togglePanel" }, -- Toggle bottom panel
+          { "n", "<leader>ur", "workbench.action.toggleAuxiliaryBar" }, -- Toggle right sidebar
         }
 
         -- Apply all keymaps
-        for _, map in ipairs(keymaps) do
-          local mode, lhs, rhs = unpack(map)
+        for _, map in pairs(keymaps) do
+          local mode, lhs, rhs = table.unpack(map)
           -- Handle both string and function values for rhs
           local cmd = type(rhs) == "function" and rhs() or vscode(rhs)
           vim.keymap.set(mode, lhs, cmd, { silent = true })
@@ -99,4 +99,4 @@ return {
       end
     end,
   },
-} 
+}
